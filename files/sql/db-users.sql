d53758e2dc3e743c42c94ce798039c0fce74773b
Lapsire <jbfernandez1612@gmail.com>
2026-03-21 00:51:27 +0100

    add sql schema

---
+++ files/sql/db-users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL
+);
