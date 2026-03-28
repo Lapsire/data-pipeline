8bdf2c0217626be75f8b997409d214a8d6be6d1a
Lapsire <jbfernandez1612@gmail.com>
2026-03-28 02:09:20 +0100

    add a first version of http_job.py

---
+++ producer/http/http_job.py
@@ -0,0 +1,49 @@
+import os
+import requests
+import time
+
+def keep_worker_files(worker_id, total_workers, path) -> list :
+    if not os.path.exists(path) :
+        raise FileNotFoundError(f"Path '{path}' does not exist.")
+    if not os.path.isdir(path):
+            raise NotADirectoryError(f"Path '{path}' is not a directory.")
+    
+    files = sorted(os.listdir(path))
+    keep = []
+
+    for file in files :
+        file = file.split('-') # Explode filename to extract part and id
+        if file[0] == "part":
+            if int(file[1]) % total_workers == worker_id : # Check if the worker will process the file
+                keep.append(os.path.join(path, "-".join(file))) # Rebuild file name
+    
+    return keep
+
+def process_file(path, url, session) :
+    with open(path, "r") as f :
+        for line in f :
+            session.post(url, line)
+
+if __name__ == "__main__" :
+    # Waiting spark to generate data
+    while 1 :
+        if os.path.exists("/data/users/_SUCCESS") : break
+        time.sleep(2)
+
+    # ENV
+    WORKER_ID = int(os.getenv("WORKER_ID",0))
+    TOTAL_WORKERS = int(os.getenv("TOTAL_WORKERS", 1))
+    API_PORT = os.getenv("API_PORT", "3000")
+    ENDPOINT = f"{os.getenv('TARGET_API', 'http://localhost')}:{API_PORT}/user"
+    
+    with requests.Session() as session :
+        session.headers.update({'Content-Type': 'application/json'})
+
+        files_to_process = keep_worker_files(WORKER_ID, TOTAL_WORKERS, "/data/users")
+
+        for f in files_to_process :
+            print(f"Processing file: {f}...", flush=True)
+            process_file(f, ENDPOINT, session)
+            print(f"File {f} processed.", flush=True)
+
+    print("Work done !", flush=True)
