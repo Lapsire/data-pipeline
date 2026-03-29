88009b3204cfe4b94df60a27be9101e8792b65bf
Lapsire <jbfernandez1612@gmail.com>
2026-03-29 03:33:16 +0200

    add redis to manage file queue for the http workers

--- producer/http/http_job.py
+++ producer/http/http_job.py
@@ -2,52 +2,58 @@ import os
 import requests
 import time
 import json
+import redis
 from datetime import datetime
 
-def keep_worker_files(worker_id, total_workers, path) -> list :
-    if not os.path.exists(path) :
-        raise FileNotFoundError(f"Path '{path}' does not exist.")
-    if not os.path.isdir(path):
-            raise NotADirectoryError(f"Path '{path}' is not a directory.")
-    
-    files = sorted(os.listdir(path))
-    keep = []
-
-    for file in files :
-        file = file.split('-') # Explode filename to extract part and id
-        if file[0] == "part":
-            if int(file[1]) % total_workers == worker_id : # Check if the worker will process the file
-                keep.append(os.path.join(path, "-".join(file))) # Rebuild file name
-    
-    return keep
-
 def process_file(path, url, session) :
-    with open(path, "r") as f :
-        for line in f :
-            data = json.loads(line)
-            data['sent_at'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            session.post(url, json=data)
+    try:
+        with open(path, "r") as f :
+            for line in f :
+                try:
+                    data = json.loads(line)
+                    data['sent_at'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+                    session.post(url, json=data)
+
+                except Exception as e:
+                    print(f"Error parsing line: {e}")
+
+    except Exception as e:
+        print(f"Could not read file {path}: {e}")
+
 
 if __name__ == "__main__" :
-    # Waiting spark to generate data
-    while 1 :
-        if os.path.exists("/data/users/_SUCCESS") : break
-        time.sleep(2)
-
     # ENV
-    WORKER_ID = int(os.getenv("WORKER_ID",0))
-    TOTAL_WORKERS = int(os.getenv("TOTAL_WORKERS", 1))
+    REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+    REDIS_PORT = os.getenv("REDIS_PORT", "6379")
     API_PORT = os.getenv("API_PORT", "3000")
     ENDPOINT = f"{os.getenv('TARGET_API', 'http://localhost')}:{API_PORT}/user"
+
+    # Connect to redis
+    r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
     
     with requests.Session() as session :
         session.headers.update({'Content-Type': 'application/json'})
 
-        files_to_process = keep_worker_files(WORKER_ID, TOTAL_WORKERS, "/data/users")
+        while True:
+            
+            if os.path.exists("/data/users/_SUCCESS"):
+                 # Create file queue
+                if r.setnx("lock", "1"):                    
+                    files = sorted(os.listdir("/data/users"))
 
-        for f in files_to_process :
-            print(f"Processing file: {f}...", flush=True)
-            process_file(f, ENDPOINT, session)
-            print(f"File {f} processed.", flush=True)
+                    for file in files:
+                        if file.startswith("part-"):
+                            r.rpush("file_queue", os.path.join("/data/users", file))
+                                        
+                    os.remove("/data/users/_SUCCESS") # Ensure no more worker will try to lock again
+                    r.delete("lock")
 
-    print("Work done !", flush=True)
+            file_bytes = r.lpop("file_queue")
+            
+            if file_bytes:
+                f = file_bytes.decode('utf-8')
+                print(f"Processing file: {f}...", flush=True)
+                process_file(f, ENDPOINT, session)
+                print(f"File {f} done.", flush=True)
+            else:
+                time.sleep(2)
