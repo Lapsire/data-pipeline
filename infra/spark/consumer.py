6cc4bd9063fe68ed77b904f36ec39eecc3d701e0
Lapsire <jbfernandez1612@gmail.com>
2026-03-30 22:42:31 +0200

    add new metrics

--- infra/spark/consumer.py
+++ infra/spark/consumer.py
@@ -1,7 +1,7 @@
 import os
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, TimestampType, IntegerType
-from pyspark.sql.functions import from_json, col, current_timestamp, expr, window, count, avg, struct
+from pyspark.sql.functions import from_json, col, current_timestamp, expr, window, count, avg, min, max
 
 # ENV
 MYSQL_PWD = os.environ.get("MYSQL_ROOT_PASSWORD", "root")
@@ -10,19 +10,54 @@ KAFKA_TOPIC_DATA = os.environ.get("KAFKA_TOPIC_DATA", "users")
 KAFKA_TOPIC_METRICS = os.getenv("KAFKA_TOPIC_METRICS", "metrics")
 BATCH_INTERVAL = os.environ.get("BATCH_INTERVAL", "2 seconds")
 
-def write_to_mysql(batch_df, batch_id):
-    """
-    Write a data batch into the database.
-    """
-    batch_df.drop("latency_seconds").write \
-        .format("jdbc") \
-        .option("url", f"jdbc:mysql://mysql:3306/{MYSQL_DB}?useSSL=false&allowPublicKeyRetrieval=true") \
-        .option("driver", "com.mysql.cj.jdbc.Driver") \
-        .option("dbtable", "users") \
-        .option("user", "root") \
-        .option("password", MYSQL_PWD) \
-        .mode("append") \
-        .save()
+def process_batch(batch_df, batch_id):
+
+    batch_df.cache()
+    
+    # Count the batch size
+    count = batch_df.count()
+
+    if count > 0 :
+        batch_df.drop("latency_seconds").write \
+            .format("jdbc") \
+            .option("url", f"jdbc:mysql://mysql:3306/{MYSQL_DB}?useSSL=false&allowPublicKeyRetrieval=true") \
+            .option("driver", "com.mysql.cj.jdbc.Driver") \
+            .option("dbtable", "users") \
+            .option("user", "root") \
+            .option("password", MYSQL_PWD) \
+            .mode("append") \
+            .save()
+
+        # Count latency for processing
+        latency_metrics = batch_df.select(
+            min("latency_seconds").alias("min_latency"),
+            max("latency_seconds").alias("max_latency"),
+            avg("latency_seconds").alias("avg_latency")
+        ).collect()[0]
+
+        # Troughput calcul
+        interval_sec = int(BATCH_INTERVAL.split(" ")[0]) 
+        throughput = count / interval_sec
+
+        # Send metrics to kafka
+        metrics = [(
+            count, 
+            throughput, 
+            latency_metrics["min_latency"],
+            latency_metrics["max_latency"],
+            latency_metrics["avg_latency"]
+        )]
+
+        column = ["count", "troughput", "min_latency", "max_latency", "avg_latency"]
+        metrics_df = spark.createDataFrame(metrics, column)
+
+        metrics_df.selectExpr("to_json(struct(*)) as value").write \
+            .format("kafka") \
+            .option("kafka.bootstrap.servers", "kafka:29092") \
+            .option("topic", "metrics") \
+            .save()
+    
+    batch_df.unpersist()
 
 spark = SparkSession.builder\
     .appName("consumer") \
@@ -55,35 +90,9 @@ parsed_df = df.selectExpr("cast(value as string)") \
     .withColumn("created_at", current_timestamp()) \
     .withColumn("latency_seconds", expr("cast(created_at as double) - cast(sent_at as double)"))
 
-# Compute metrics
-metrics_df = parsed_df \
-    .withWatermark("sent_at", "1 minute") \
-    .groupBy(
-        window(col("sent_at"), "1 minute"), 
-        col("city")
-    ) \
-    .agg(
-        count("*").alias("volume"), 
-        avg("latency_seconds").alias("avg_latency")
-    )
-
-# Transform metrics df for kafka
-kafka_metrics_df = metrics_df.selectExpr(
-        "cast(window.start as string) as key",
-        "to_json(struct(*)) as value"
-    )
-
-query_metrics = kafka_metrics_df.writeStream \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", "kafka:29092") \
-    .option("topic", KAFKA_TOPIC_METRICS) \
-    .option("checkpointLocation", "/tmp/spark_checkpoints_metrics") \
-    .outputMode("update") \
-    .start()
-
 # Write data flux in db  
 query = parsed_df.writeStream \
-.foreachBatch(write_to_mysql) \
+.foreachBatch(process_batch) \
 .option("checkpointLocation", "/tmp/spark_checkpoints_mysql") \
 .trigger(processingTime=BATCH_INTERVAL) \
 .start()
